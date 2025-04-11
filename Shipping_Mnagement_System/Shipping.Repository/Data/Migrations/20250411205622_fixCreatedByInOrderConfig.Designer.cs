3b7a3393cdb2138205958b12ea3b5af824e41dc2
iamhashemtarek <hashemtarek2012@gmail.com>
2025-04-11 23:07:38 +0200

    fix getOrders endpoint

--- Shipping_Mnagement_System/Shipping.Repository/Data/Migrations/20250411205622_fixCreatedByInOrderConfig.Designer.cs
+++ Shipping_Mnagement_System/Shipping.Repository/Data/Migrations/20250411205622_fixCreatedByInOrderConfig.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shipping.Repository.Data;
 
@@ -11,9 +12,11 @@ using Shipping.Repository.Data;
 namespace Shipping.Repository.Data.Migrations
 {
     [DbContext(typeof(ShippingContext))]
-    partial class ShippingContextModelSnapshot : ModelSnapshot
+    [Migration("20250411205622_fixCreatedByInOrderConfig")]
+    partial class fixCreatedByInOrderConfig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Shipping_Mnagement_System/Shipping.Repository/Data/Migrations/20250411205622_fixCreatedByInOrderConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shipping.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixCreatedByInOrderConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "EmployeeId",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmployeeId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Employees_EmployeeId",
+                table: "Orders",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id");
+        }
+    }
+}
